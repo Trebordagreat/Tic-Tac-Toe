8d0e949398480d7ce7b2434abe8b30b5d9894c13
Tye Rooth <tyerooth@gmail.com>
2021-12-11 16:20:59 -0500

    added reset function and fixed game not stopping bug

--- script.js
+++ script.js
@@ -1,11 +1,100 @@
+
+const setUpPage = (() => {
+
+    const retrieveName = () => {
+        let nameField = document.createElement('input');
+        nameField.setAttribute('placeholder', 'Name');
+        nameField.classList.add('playerName');
+        document.querySelector('.game').appendChild(nameField);
+    }
+
+    const generatePage = () => {
+        retrieveName();
+
+    }
+
+    return {
+        retrieveName
+    };
+})();
+
+setUpPage.retrieveName()
+
+const Player = (symbol) => {
+    const winCheck = () => {
+        const winConditions = ['row0', 'row1', 'row2', 'col0', 'col1', 'col2', 'TL-BR', 'BL-TR'];
+        for (let i = 0; i < winConditions.length; i++) {
+            let matchingSymbol = 0;
+            const winConditionCells = document.querySelectorAll(`.${ winConditions[i] }`);
+            winConditionCells.forEach(cell => {
+                if (cell.textContent === symbol) {
+                    matchingSymbol++;
+                }
+            });
+            if (matchingSymbol === 3) {
+                return true;
+            }
+        }
+    }
+
+    return {
+        symbol,
+        winCheck
+    };
+}
+
+const xPlayer = Player('X');
+const oPlayer = Player('O');
+
+const gameFlow = (() => {
+    let gameTurn = 1;
+
+    const newGame = () => {
+        gameTurn = 1;
+    }
+
+    const currentPlayer = () => {
+        if (gameTurn % 2 === 1) {
+            return xPlayer;
+        }
+        else {
+            return oPlayer;
+        }
+    }
+
+    const alternatePlayers = () => {
+        endGame(); 
+        gameTurn++;
+    }
+
+    const endGame = () => {
+        const resultMessage = document.createElement('div');
+        resultMessage.classList.add('results');
+        
+        if (currentPlayer().winCheck() === true) {
+            resultMessage.textContent = `${ currentPlayer().symbol } has won the match`;
+        }
+        else if (gameTurn === 9) {
+            resultMessage.textContent = "The match was a tie";
+        }
+        document.querySelector('.game').appendChild(resultMessage);
+    }
+
+    return {
+        currentPlayer,
+        alternatePlayers,
+        newGame
+    }
+})();
+
 const gameBoard = (() => {
     const gameBoardArray = ['','','','','','', '', '', ''];
 
     const markSymbol = () => {
         document.querySelectorAll('td').forEach((cell, index) => {
 
-            if (cell.classList.contains('openCell')) {
-                cell.addEventListener('click', () => {
+            if (cell.classList.contains('openCell') && gameFlow.currentPlayer().winCheck() !== true) {
+                cell.addEventListener('click', function markCell () {
                     gameBoardArray[index] = gameFlow.currentPlayer().symbol;
                     createBoard();
                     gameFlow.alternatePlayers();
@@ -14,10 +103,30 @@ const gameBoard = (() => {
         });    
     }
 
+    const resetBoard = () => {
+        const resetButton = document.createElement('button');
+        resetButton.textContent = "Reset Game";
+        resetButton.classList.add('reset');
+        resetButton.addEventListener('click', () => {
+            for (let i = 0; i < gameBoardArray.length; i++) {
+                gameBoardArray[i] = "";
+            }
+            createBoard();
+            gameFlow.newGame();
+        });
+        return resetButton;
+    }
+
     const createBoard = () => {
+        const page = document.querySelector('.game');
+
         if (previousBoard = document.querySelector('table')) {
-            document.querySelector('.game').removeChild(previousBoard);
+            page.removeChild(previousBoard);
+            page.removeChild(document.querySelector('.reset'));
         };
+        if (previousVictory = document.querySelector('.results')) {
+            page.removeChild(previousVictory);
+        }
         let board = document.createElement('table')
         for (let i = 0; i < 3; i++) {
             let boardRow = document.createElement('tr')
@@ -48,74 +157,16 @@ const gameBoard = (() => {
             }
             board.appendChild(boardRow);
         }
-        document.querySelector('.game').appendChild(board);
+        const resetButton = resetBoard();
+
+        page.appendChild(board);
+        page.appendChild(resetButton);
         markSymbol();
     }
     return {
         createBoard,
-        gameBoardArray,
-        markSymbol
+        gameBoardArray
     };
 })();
 
 gameBoard.createBoard();
-
-
-
-const Player = (symbol) => {
-    const winCheck = () => {
-        const winConditions = ['row0', 'row1', 'row2', 'col0', 'col1', 'col2', 'TL-BR', 'BL-TR'];
-        for (let i = 0; i < winConditions.length; i++) {
-            let matchingSymbol = 0;
-            const winConditionCells = document.querySelectorAll(`.${ winConditions[i] }`);
-            winConditionCells.forEach(cell => {
-                if (cell.textContent === symbol) {
-                    matchingSymbol++;
-                }
-            });
-            if (matchingSymbol === 3) {
-                return true;
-            }
-        }
-    }
-
-    return {
-        symbol,
-        winCheck
-    };
-}
-
-const me = Player('X');
-const computer = Player('O');
-
-const gameFlow = (() => {
-    let gameTurn = 1;
-
-    const currentPlayer = () => {
-        if (gameTurn % 2 === 1) {
-            return me;
-        }
-        else {
-            return computer;
-        }
-    }
-
-    const alternatePlayers = () => {
-        endGame(); 
-        gameTurn++;
-    }
-
-    const endGame = () => {
-        if (currentPlayer().winCheck() === true) {
-            console.log(`${ currentPlayer().symbol } has won the match`);
-        }
-        else if (gameTurn === 9) {
-            console.log("The match was a tie");
-        }
-    }
-
-    return {
-        currentPlayer,
-        alternatePlayers
-    }
-})();
